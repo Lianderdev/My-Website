487b19ae6bae8f11f77ea2c8adcaa75bbd38e05f
liander <liandervinicius13@gmail.com>
2024-11-17 21:45:21 -0300

    feat: create home component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { Header } from "./components/layoult/Header";
+import { Home } from "./sections/Home";
 
 export function App() {
   return (
     <>
       <Header />
       <main>
-
+        <Home />
       </main>
     </>
   )
