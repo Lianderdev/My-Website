fbb75a17d120ff48ff4850fbb3b5e3a2da102d48
Liander <liandervinicius13@gmail.com>
2024-11-21 12:54:12 -0300

    feat(contact): add form submission functionality with EmailJS

--- src/sections/Contacts.jsx
+++ src/sections/Contacts.jsx
@@ -1,10 +1,44 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "../components/ui/Input";
-import { motion } from "framer-motion"
-
+import emailjs from 'emailjs-com';
 import { RiContactsBookLine } from "react-icons/ri";
+import { useRef, useState } from "react";
+
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
 
 export function Contact() {
+    const nameRef = useRef()
+    const emailRef = useRef()
+    const messageRef = useRef()
+
+    const [isLoading, setIsLoading] = useState(false)
+    const [modalSucess, setModalSucess] = useState(false);
+    const [modalFailed, setModalFailed] = useState(false);
+
+    function sendEmail(event) {
+        event.preventDefault()
+
+        setIsLoading(true)
+
+        const name = nameRef.current?.value?.trim();
+        const email = emailRef.current?.value?.trim();
+        const message = messageRef.current?.value?.trim();
+
+        if (name && email && message) {
+            const templateParams = {from_name: name, message: message, email: email}
+
+            emailjs.send('service_s8dqj6i', 'template_30r3d2g', templateParams, 'dyXZYpcE0VEwEM0vN')
+            .then(() => {
+                setModalSucess(true)
+            }).catch((err) => {
+                setModalFailed(true)
+            }).finally(() => {
+                setIsLoading(false)
+            })
+        }
+
+    }
+
     return (
         <section id="contacts">
             <div className="flex items-center gap-2 text-base sm:text-lg md:text-xl">
@@ -12,10 +46,10 @@ export function Contact() {
                 <h2>Contatos</h2>
             </div>
 
-            <form className='flex flex-col gap-4 border bg-zinc-950 p-4 rounded-lg'>
+            <form onSubmit={sendEmail} className='flex flex-col gap-4 border bg-zinc-950 p-4 rounded-lg'>
                 <div className='grid grid-cols-[repeat(auto-fit,_minmax(300px,1fr))] gap-4'>
-                    <Input label="name" type="text" id="name" aria-label="Nome" placeholder="Digite seu nome" required />
-                    <Input label="Email" type="text" id="e-mail" aria-label="Email" placeholder="Digite seu E-mail" required />
+                    <Input label="name" type="text" name="Name" ref={nameRef} id="name" aria-label="Nome" placeholder="Digite seu nome" required />
+                    <Input label="Email" type="email" name="Email" ref={emailRef} id="e-mail" aria-label="Email" placeholder="Digite seu E-mail" required />
                 </div>
                 <div className='flex flex-col gap-2 text-xs md:text-sm'>
                     <label htmlFor="message">Mensagem</label>
@@ -27,9 +61,21 @@ export function Contact() {
                         placeholder="Digite sua mensagem"
                         required
                         aria-label="Mensagem"
+                        ref={messageRef}
                     />
                 </div>
-                <Button>Enviar Mensagem</Button>
+                <Button type='submit' className="text:sm md:text-base">
+                    {isLoading ? 
+                    (<div className="flex items-center gap-2">
+                        <AiOutlineLoading3Quarters className="isloading"/>
+                        <p className="font-bold">Carregando</p>
+                    </div>)
+                    : 
+                    (<p className="font-bold">
+                        Enviar Mensagem
+                    </p>)
+                    }
+                </Button>
             </form>
 
             <p className="text-xs md:text-sm text-zinc-400">Ou, se preferir, entre em contato por meio destas redes sociais: 
