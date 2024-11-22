ea676d92fa8d50777e97b9d7abf163fd567db0b5
Liander <liandervinicius13@gmail.com>
2024-11-22 12:36:05 -0300

    feat: create Experiences components

--- src/components/ui/accordion.jsx
+++ src/components/ui/accordion.jsx
@@ -16,13 +16,14 @@ const AccordionTrigger = React.forwardRef(({ className, children, ...props }, re
     <AccordionPrimitive.Trigger
       ref={ref}
       className={cn(
-        "flex flex-1 items-center justify-between py-4 text-sm font-medium transition-all text-left [&[data-state=open]>svg]:rotate-180",
+        "flex flex-col gap-3 flex-1 justify-between py-4 text-sm font-medium transition-all text-left [&[data-state=open]>svg]:rotate-180  relative",
         className
       )}
       {...props}>
       {children}
-      <ChevronDown
-        className="h-4 w-4 shrink-0 text-muted-foreground transition-transform duration-200" />
+
+      <ChevronDown className="h-4 w-4 shrink-0 text-muted-foreground transition-transform duration-200 " />
+      
     </AccordionPrimitive.Trigger>
   </AccordionPrimitive.Header>
 ))
--- src/sections/Experiences.jsx
+++ src/sections/Experiences.jsx
@@ -1,5 +1,6 @@
 import { BsSuitcaseLg } from "react-icons/bs";
 import inmetro from "../assets/img/inmetro.jpg"
+import ifpa from "../assets/img/if.png"
 
 import {
     Accordion,
@@ -18,22 +19,41 @@ export function Experiences() {
 
             <Accordion type="single" collapsible className="w-full">
                 <AccordionItem value="item-1">
-                    <AccordionTrigger>
-                        <div className="flex flex-col">
-                            <h3>Estágiario de Ti</h3>
-                            <p>out 2024 - Present</p>
-                        </div>
-                        <div>
-                            <div>
-                                <img src="" alt="" />
+                    <AccordionTrigger className="border-t relative">
+                        <div className="w-full flex justify-between items-center">
+                            <div className="flex flex-col gap-2">
+                                <h3 className="text-base">Estágiario de Ti</h3>
+                                <p className="text-xs capitalize text-zinc-200">out 2024 - present</p>
                             </div>
-                            <p>Inmetro</p>
-                            <span>Presencial</span>
+                            <div className="flex items-center gap-2">
+                                <div className="w-8 h-8 rounded-lg overflow-hidden">
+                                    <img src={inmetro} alt={`logo do ${inmetro}`} className="w-full object-cover" />
+                                </div>
+                                <div className="flex flex-col">
+                                    <spam>Inmetro</spam>
+                                    <span className="font-normal text-zinc-200">Presencial</span>
+                                </div>
+                            </div>
                         </div>
+                        <p className="absolute bottom-[13px] left-6 text-zinc-500 hover:text-zinc-50 duration-200 transition-colors">Experiências passadas</p>
                     </AccordionTrigger>
 
                     <AccordionContent>
-                        jszsja
+                        <div className="w-full flex justify-between items-center">
+                            <div className="flex flex-col gap-2">
+                                <h3 className="text-base">Monitor do Laborátorio de Informática</h3>
+                                <p className="text-xs capitalize text-zinc-200">mar 2022 - out 2022</p>
+                            </div>
+                            <div className="flex items-center gap-2">
+                                <div className="w-8 h-8 flex items-center justify-center flex-shrink-0 bg-zinc-50 rounded-lg overflow-hidden mt-1">
+                                    <img src={ifpa} alt={`logo do ${ifpa}`} />
+                                </div>
+                                <div className="flex flex-col">
+                                    <spam className="text-xs text-blue-600">Inmetro</spam>
+                                    <span className="text-xs text-zinc-200">Presencial</span>
+                                </div>
+                            </div>
+                        </div>
                     </AccordionContent>
                 </AccordionItem>
             </Accordion>
