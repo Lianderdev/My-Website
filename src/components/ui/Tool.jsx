4a4e6c4a5b501c0365283285a8eefe64ee819dcd
Liander <liandervinicius13@gmail.com>
2024-11-22 12:39:29 -0300

    style: changes to tool styles

--- src/components/ui/Tool.jsx
+++ src/components/ui/Tool.jsx
@@ -1,11 +1,10 @@
 export function Tool({ name, color, icon }) {
     return (
-        <li style={{ userSelect: 'none' }} className="min-w-[113px] flex flex-col justify-center rounded-lg transition-colors border bg-zinc-900 flex-1 py-2">
-            <div style={{ color: color }} className='p-1 md:p-2 mx-auto mb-2 rounded-md text-2xl relative border w-10 h-10 md:w-12 md:h-12 overflow-hidden'>
-                <img src={icon} alt="" className="absolute top-0 left-0 blur-xl"/>
-                <img src={icon} alt="" className="w-full"/>
-            </div>
-            <div className="border-t pl-2 pt-2">
+        <li style={{ userSelect: 'none' }} className="min-w-[130px] flex items-center gap-2 flex-1 p-2 rounded-lg transition-colors bg-zinc-900">
+            <div style={{ color: color }} className='rounded-md p-1 text-2xl relative border w-10 h-10 md:w-8 md:h-8 overflow-hidden flex-shrink-0'>
+                <img src={icon} alt="" className="absolute top-0 left-0 blur-xl" />
+                <img src={icon} alt="" className="w-full" />
+            </div> <div className="">
                 <p className="text-[10px] font-semibold uppercase tracking-wider">{name}</p>
             </div>
         </li>
